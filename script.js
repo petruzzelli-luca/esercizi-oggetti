f9a489d630a7cefdd6fa70ca81f9ef60f0b570f7
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2025-01-29 10:46:27 +0000

    lavoro finale

---
+++ script.js
@@ -0,0 +1,10 @@
+const mucca = {
+    zampe: 4,
+    colore : "bianca e nera",
+    razza:  "felina"
+  }
+
+function mucca(){
+    document.getElementById("mucca").innerHTML= `Zampe: ${mucca.zampe}, Colore: ${mucca.colore}, Razza: ${mucca.razza}`;
+}
+
