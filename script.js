b1a6ac7dcc04b6994fea9726f84a22019fd9aab2
petruzzelli-luca <petruzzelli.luca@iisgalvanimi.edu.it>
2025-01-31 10:40:18 +0000

    lavoro finale 2.0

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-const mucca = {
+var mucca = {
     zampe: 4,
     colore : "bianca e nera",
-    razza:  "felina"
+    razza:  "chianina"
   }
 
-function mucca(){
-    document.getElementById("mucca").innerHTML= `Zampe: ${mucca.zampe}, Colore: ${mucca.colore}, Razza: ${mucca.razza}`;
-}
+  var maiale = {
+    zampe: 4,
+    colore : "rosa",
+    razza:  "largewhite"
+  }
 
+  var gallo = {
+    zampe: 2,
+    colore : "rosso e bianco",
+    razza:  "bionda piemontese"
+  }
+
+function mucca1(){
+    document.getElementById("mucca").innerHTML=`Zampe: ${mucca.zampe} <br\> Colore: ${mucca.colore}<br\> Razza: ${mucca.razza}`;
+}
+function gallo1(){
+  document.getElementById("gallo").innerHTML=`Zampe: ${gallo.zampe} <br\> Colore: ${gallo.colore}<br\> Razza: ${gallo.razza}`;
+}
+function maiale1(){
+  document.getElementById("maiale").innerHTML=`Zampe: ${maiale.zampe} <br\> Colore: ${maiale.colore}<br\> Razza: ${maiale.razza}`;
+}
